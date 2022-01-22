0153ad46cbf16d4fc31f681840d82ba8c2fb3a26
zhaolong <naokimidori@163.com>
2022-01-22 18:20:15 +0800

    feat: update article validate

--- validator/article.js
+++ validator/article.js
@@ -1,6 +1,7 @@
 const { body, param } = require('express-validator')
 const validate = require('../middleware/validate')
 const mongoose = require('mongoose')
+const { Article, User } = require('../model')
 
 exports.createArticle = validate([
   body('article.title').notEmpty().withMessage('文章标题不能为空'),
@@ -17,6 +18,26 @@ exports.getArticle = validate([
   validate.isValidObjectId(['params'], 'articleId')
 ])
 
-exports.updateArticle = validate([
-  validate.isValidObjectId(['params'], 'articleId')
-])
+exports.updateArticle = [
+  // 校验ID是否合法
+  validate([
+    validate.isValidObjectId(['params'], 'articleId')
+  ]),
+  // 校验文章是否存在
+  async (req, res, next) => {
+    const articleId = req.params.articleId
+    const article = await Article.findById(articleId)
+    if (!article) {
+      return res.status(404).end()
+    }
+    req.article = article
+    next()
+  },
+  // 校验是否是作者本人
+  async (req, res, next) => {
+    if (req.user._id.toString() !== req.article.author.toString()) {
+      return res.status(403).end()
+    }
+    next()
+  }
+]
